bd560992dec20dcd2a3228483a82f90eaef10306
Davide Pugliese <dvd.pugliese@gmail.com>
2017-08-20 09:13:32 +0300

    included some dependencies I forgot in package.json

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
-import './App.css';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
